fee118c5a96b9f294420a4982fb15547f833aeaa
Jaswinder Singh <jaswinsingh@deloitte.com>
2019-09-04 21:23:03 +0530

    Component Added

---
+++ force-app/main/default/aura/scratchOrgLimits/scratchOrgLimitsController.js
@@ -0,0 +1,12 @@
+({
+    getdetailsonload: function(cmp) {
+        var action = cmp.get("c.getScratchOrgDetailsfulllist");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                cmp.set("v.wrapperList", response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+    }
+});
